ce8bb850af5a3cda31f5a102087e3b1805903a9a
Lam Tran <lam.tran@commercetools.de>
2017-12-06 16:03:33 +0100

    #14 edit test Example 5

--- spec/integration/runner/variant-reassignment-example-5.spec.js
+++ spec/integration/runner/variant-reassignment-example-5.spec.js
@@ -4,27 +4,29 @@ import VariantReassignment from '../../../lib/runner/variant-reassignment'
 
 /* eslint-disable max-len */
 /**
- * +-------------------------------------------------+---------------------------+--------------------+-------------------------------------------------+
- * | New product draft                               | CTP product               | After reassignment | CTP product                                     |
- * +-------------------------------------------------+---------------------------+                    +-------------------------------------------------+
- * | Product:                                        | Product:                  |                    | Product:                                        |
- * | slug: { en: "product-xxx", de: "produkte-xxx" } | id: "1"                   |                    | id: "1"                                         |
- * | product-type: "pt2"                             | slug: { en: "product-1" } |                    | slug: { en: "product-1" }                       |
- * | variants: v1, v3                                | product-type: "pt2"       |                    | product-type: "pt2"                             |
- * |                                                 | variants: v1, v2          |                    | variants: v2                                    |
- * +-------------------------------------------------+---------------------------+                    +-------------------------------------------------+
- * |                                                 | Product:                  |                    | Product:                                        |
- * |                                                 | id: "2"                   |                    | id: "2"                                         |
- * |                                                 | slug: { en: "product-2" } |                    | slug: { en: "product-2" }                       |
- * |                                                 | product-type: "pt2"       |                    | product-type: "pt2"                             |
- * |                                                 | variants: v3, v4          |                    | variants: v4                                    |
- * +-------------------------------------------------+---------------------------+--------------------+-------------------------------------------------+
- * |                                                 |                           |                    | Product:                                        |
- * |                                                 |                           |                    | id: "3"                                         |
- * |                                                 |                           |                    | slug: { en: "product-xxx", de: "produkte-xxx" } |
- * |                                                 |                           |                    | product-type: "pt2"                             |
- * |                                                 |                           |                    | variants: v1, v3                                |
- * +-------------------------------------------------+---------------------------+--------------------+-------------------------------------------------+
+ +-------------------------------------------------+---------------------------+--------------------+---------------------------------------------------------------+
+ | New product draft                               | CTP product               | After reassignment | CTP product                                                   |
+ +-------------------------------------------------+---------------------------+                    +---------------------------------------------------------------+
+ | Product:                                        | Product:                  |                    | Product:                                                      |
+ | slug: { en: "product-xxx", de: "produkte-xxx" } | id: "1"                   |                    | id: "1"                                                       |
+ | product-type: "pt2"                             | slug: { en: "product-1" } |                    | slug: { en: "product-1" }                                     |
+ | masterVariant: v1                               | product-type: "pt2"       |                    | product-type: "pt2"                                           |
+ | variants: v3                                    | masterVariant: v1         |                    | masterVariant: v1                                             |
+ |                                                 | variants: v2              |                    | variants: v3                                                  |
+ +-------------------------------------------------+---------------------------+                    +---------------------------------------------------------------+
+ |                                                 | Product:                  |                    | Product:                                                      |
+ |                                                 | id: "2"                   |                    | id: "2"                                                       |
+ |                                                 | slug: { en: "product-2" } |                    | slug: { en: "product-2" }                                     |
+ |                                                 | product-type: "pt2"       |                    | product-type: "pt2"                                           |
+ |                                                 | masterVariant: v3         |                    | variants: v4                                                  |
+ |                                                 | variants: v4              |                    |                                                               |
+ +-------------------------------------------------+---------------------------+                    +---------------------------------------------------------------+
+ |                                                 |                           |                    | Product:                                                      |
+ |                                                 |                           |                    | id: "3"                                                       |
+ |                                                 |                           |                    | slug: { en: "product-1_${timestamp}", _ctsd: "${timestamp}" } |
+ |                                                 |                           |                    | product-type: "pt2"                                           |
+ |                                                 |                           |                    | masterVariant: v2                                             |
+ +-------------------------------------------------+---------------------------+--------------------+---------------------------------------------------------------+
  */
 /* eslint-enable max-len */
 describe('Variant reassignment', () => {
@@ -71,17 +73,17 @@ describe('Variant reassignment', () => {
 
       const { body: { results } } = await utils.getProductsBySkus(['1', '2', '3', '4'], ctpClient)
       expect(results).to.have.lengthOf(3)
-      const updatedProduct1 = results.find(product => product.masterVariant.sku === '2')
-      const updatedProduct2 = results.find(product => product.masterVariant.sku === '4')
-      const newProduct3 = results.find(product => product.masterVariant.sku === '1'
-        || product.masterVariant.sku === '3')
-      expect(updatedProduct1.variants).to.have.lengthOf(0)
+      const updatedProduct1 = results.find(product => product.masterVariant.sku === '1')
+      expect(updatedProduct1.variants).to.have.lengthOf(1)
+      expect(updatedProduct1.variants[0].sku).to.equal('3')
       expect(updatedProduct1.id).to.equal(product1.id)
 
+      const updatedProduct2 = results.find(product => product.masterVariant.sku === '4')
       expect(updatedProduct2.variants).to.have.lengthOf(0)
       expect(updatedProduct2.id).to.equal(product2.id)
 
-      expect(newProduct3.variants).to.have.lengthOf(1)
-      expect(newProduct3.slug.en).to.equal(productDraft.slug.en)
+      const newProduct3 = results.find(product => product.masterVariant.sku === '2')
+      expect(newProduct3.variants).to.have.lengthOf(0)
+      expect(newProduct3.slug._ctsd).to.be.a('string')
     })
 })
