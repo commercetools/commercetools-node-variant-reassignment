0da25df986d5702c886b5a743d81c236b40e0302
Jan Juna <mail@janjuna.cz>
2018-01-05 15:28:41 +0100

    Add test for failed processing before transaction is created #3

--- spec/integration/error-cases/reassignment-error.spec.js
+++ spec/integration/error-cases/reassignment-error.spec.js
@@ -16,6 +16,7 @@ describe('Reassignment error', () => {
   before(async () => {
     ctpClient = await utils.createClient()
   })
+
   beforeEach(async () => {
     logger = utils.createLogger(__filename)
     spyError = sinon.spy(logger, 'error')
@@ -79,6 +80,51 @@ describe('Reassignment error', () => {
       expect(spyError.callCount).to.equal(1)
       expect(spyError.lastCall.args[1].toString()).to.contain('test error')
 
+      return Promise.resolve()
+    }
+  })
+
+  it('should retry when reassignment fails before creating transaction', async () => {
+    const spyUnfinished = sinon.spy(reassignment, '_processUnfinishedTransactions')
+    const spyProductDraft = sinon.spy(reassignment, '_processProductDraft')
+
+    sinon.stub(reassignment, '_selectMatchingProducts')
+      .onFirstCall().rejects('test error')
+      .callThrough()
+
+    await reassignment.execute([productDraft], [product1, product2])
+
+    expect(spyError.callCount).to.equal(1)
+    expect(spyError.firstCall.args[0])
+      .to.contain('Error while processing productDraft')
+    expect(spyError.firstCall.args[2])
+      .to.contain('test error')
+
+    expect(spyUnfinished.callCount).to.equal(1)
+    expect(spyProductDraft.callCount).to.equal(2)
+
+    const { body: { results } } = await utils.getProductsBySkus(['1', '2', '3', '4'], ctpClient)
+    expect(results).to.have.lengthOf(3)
+    const backupProduct = results.find(product => product.masterVariant.sku === '4')
+    expect(backupProduct).to.be.an('object')
+    expect(backupProduct.variants).to.have.lengthOf(0)
+    const updatedProduct = results.find(product => product.masterVariant.sku === '1')
+    expect(updatedProduct.variants[0].sku).to.equal(productDraft.variants[0].sku)
+
+    const anonymizedProduct = results.find(product => product.masterVariant.sku === '2')
+    expect(anonymizedProduct).to.be.an('object')
+    expect(anonymizedProduct.slug).to.haveOwnProperty('ctsd')
+  })
+
+  it('should retry only once when reassignment fails before creating transaction', async () => {
+    sinon.stub(reassignment, '_selectMatchingProducts').rejects('test error')
+
+    try {
+      await reassignment.execute([productDraft], [product1, product2])
+      return Promise.reject('Should throw an error')
+    } catch (e) {
+      expect(e.toString()).to.contain('test error')
+      expect(spyError.callCount).to.equal(1)
       return Promise.resolve()
     }
   })
